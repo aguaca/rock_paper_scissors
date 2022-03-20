11f64e2d15ee9fe79f77f9e6dab1c62a08223fd4
Isaias Geronimo <mprii@hotmail.com>
2022-03-19 20:05:27 -0400

    AI vs AI plus validation

--- rps.py
+++ rps.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+import random
+
 """This program plays a game of Rock, Paper, Scissors between two Players,
 and reports both Player's scores each round."""
 
@@ -5,6 +8,16 @@ moves = ['rock', 'paper', 'scissors']
 
 """The Player class is the parent class for all of the Players
 in this game"""
+
+
+def valid_input(prompt="Rock, paper, scissors? > "):
+    while True:
+        response = input(prompt).lower()
+        if response in moves:
+            break
+        else:
+            print("Sorry, I don't understand.")
+    return response
 
 
 class Player:
@@ -15,6 +28,11 @@ class Player:
         pass
 
 
+class RandomPlayer(Player):
+    def move(self):
+        return random.choice(moves)
+
+
 def beats(one, two):
     return ((one == 'rock' and two == 'scissors') or
             (one == 'scissors' and two == 'paper') or
@@ -22,25 +40,35 @@ def beats(one, two):
 
 
 class Game:
-    def __init__(self, p1, p2):
+    def __init__(self, p1, p2): 
         self.p1 = p1
         self.p2 = p2
 
     def play_round(self):
         move1 = self.p1.move()
         move2 = self.p2.move()
-        print(f"Player 1: {move1}  Player 2: {move2}")
+        print(f"You played {move1}.")
+        print(f"Opponent played {move2}.")
+
+        result = beats(move1, move2)
+        if move1 == move2:
+            print("** TIE **")
+        elif result:
+            print("** PLAYER ONE WINS **")
+        else:
+            print("** PLAYER TWO WINS **")
+
         self.p1.learn(move1, move2)
         self.p2.learn(move2, move1)
 
     def play_game(self):
         print("Game start!")
         for round in range(3):
-            print(f"Round {round}:")
+            print(f"Round {round} --")
             self.play_round()
         print("Game over!")
 
 
 if __name__ == '__main__':
-    game = Game(Player(), Player())
+    game = Game(RandomPlayer(), RandomPlayer())
     game.play_game()
